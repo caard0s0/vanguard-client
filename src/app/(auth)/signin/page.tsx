c60768d26b243f88a14850ec23288ef391812b54
caard0s0 <vinicardoso216@gmail.com>
2023-10-03 18:05:35 -0300

    feat: building auth (signup/signin) pages

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -3,6 +3,7 @@
 import { useRouter } from 'next/navigation'
 import { ChangeEvent, FormEvent, useState } from 'react'
 import { listAccounts, loginUser } from '@/app/api/routes'
+import Image from 'next/image'
 
 export interface UserSignIn {
   username: string
@@ -38,29 +39,72 @@ export default function SignIn() {
   }
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Enter your username</label>
-          <input
-            type="text"
-            name="username"
-            value={user.username}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label>Enter your password</label>
-          <input
-            type="password"
-            name="password"
-            value={user.password}
-            onChange={handleChange}
-          />
-        </div>
+    <div className="flex h-screen flex-col items-center bg-[linear-gradient(215deg,_#171d26_15%,_#000_85%)]">
+      <header className="mt-6 grid place-items-center">
+        <Image
+          src="/united_atomic_bank_logo_white.svg"
+          alt="United Atomic Bank Logo"
+          width={100}
+          height={100}
+        />
+        <h1 className="mt-2 text-xl text-white">Sign In Form</h1>
+      </header>
 
-        <button type="submit">Submit</button>
-      </form>
+      <main>
+        <div className="mt-5 bg-[#1c2026] p-10">
+          <form
+            className=" flex flex-col gap-6 text-white"
+            onSubmit={handleSubmit}
+          >
+            <div>
+              <label className="block text-sm text-white opacity-80">
+                Enter your Username
+              </label>
+              <input
+                className="border-b-2 border-[#78777f] bg-transparent py-2 text-base outline-0 focus:border-[#00ffff] focus:transition-all"
+                type="text"
+                name="username"
+                value={user.username}
+                onChange={handleChange}
+                placeholder="Enter your Username"
+                autoComplete="off"
+                spellCheck="false"
+                required
+                autoFocus
+                size={42}
+                minLength={4}
+                maxLength={256}
+              />
+            </div>
+            <div>
+              <label className="block text-sm text-white opacity-80">
+                Enter your Password
+              </label>
+              <input
+                className="border-b-2 border-[#78777f] bg-transparent py-2 text-base outline-0 focus:border-[#00ffff] focus:transition-all"
+                type="password"
+                name="password"
+                value={user.password}
+                onChange={handleChange}
+                placeholder="Enter your Password"
+                autoComplete="off"
+                spellCheck="false"
+                required
+                size={42}
+                minLength={4}
+                maxLength={256}
+              />
+            </div>
+
+            <button
+              className="mt-2 min-w-full rounded bg-gray-500 px-4 py-2 text-black transition-all hover:bg-gray-400"
+              type="submit"
+            >
+              Login User
+            </button>
+          </form>
+        </div>
+      </main>
     </div>
   )
 }
