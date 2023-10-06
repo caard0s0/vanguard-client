497d876f82aadaeb5894cbd942537fe39a825431
caard0s0 <vinicardoso216@gmail.com>
2023-10-06 09:51:37 -0300

    feat: building bank accounts page

--- src/app/accounts/page.tsx
+++ src/app/accounts/page.tsx
@@ -1,7 +1,52 @@
 import { listAccounts } from '../api/routes'
+import { User2, Eye, Dot } from 'lucide-react'
 
 export default async function Accounts() {
-  const accountData = await listAccounts()
+  const [accountData] = await listAccounts()
 
-  return <div>{<pre>{JSON.stringify(accountData, null, 2)}</pre>}</div>
+  return (
+    <div className="flex h-screen flex-col overflow-scroll bg-[linear-gradient(215deg,_#171d26_15%,_#000_85%)]">
+      <div className="mx-12">
+        <header className="mt-10 flex items-center justify-between">
+          <Eye size={30} color="white" />
+          <h1 className="text-4xl font-semibold text-white">UAB</h1>
+          <User2 size={30} color="white" />
+        </header>
+
+        <div className="mb-8 mt-16 rounded-3xl bg-white p-5">
+          <div className="mt-2 flex items-center">
+            <Dot size={40} className="mt-1" color="black" />
+            <strong className="text-xl text-black">{accountData.id}</strong>
+          </div>
+
+          <div className="ml-4 mt-20">
+            <h2 className="text-xl font-bold text-gray-950">Balance</h2>
+            <strong className="mt-3 block text-3xl text-black">
+              $ {accountData.balance},00
+            </strong>
+            <strong className="mt-7 block font-extrabold text-gray-800">
+              {accountData.owner}
+            </strong>
+          </div>
+          <a
+            href="/transfer"
+            className="mt-10 block min-w-full rounded-2xl bg-black p-6 text-center text-white hover:bg-gray-950"
+          >
+            Send Money
+          </a>
+        </div>
+
+        <div>
+          <h2 className="text-xl font-bold text-white">Transactions</h2>
+          <div className="my-3 rounded-3xl bg-white px-6 py-3">
+            <div className="flex items-center justify-between">
+              <strong className="text-lg">João Pedro</strong>
+              <strong className="text-lg">$ 5.99</strong>
+            </div>
+            <span className="text-sm">20 FEV 2023</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
