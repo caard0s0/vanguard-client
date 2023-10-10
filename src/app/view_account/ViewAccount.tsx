2bc28d104aa613a710b5bd9b47ff849a7ed72cdd
caard0s0 <vinicardoso216@gmail.com>
2023-10-10 16:37:29 -0300

    feat: adding icon to toggle balance

--- src/app/view_account/ViewAccount.tsx
+++ src/app/view_account/ViewAccount.tsx
@@ -1,6 +1,13 @@
 'use client'
 
-import { User2, Eye, Dot } from 'lucide-react'
+import {
+  Eye,
+  EyeClosed,
+  User,
+  DotOutline,
+  DotsThreeOutline,
+} from '@phosphor-icons/react'
+import { useState } from 'react'
 
 export interface ViewAccountProps {
   id: number
@@ -9,26 +16,47 @@ export interface ViewAccountProps {
 }
 
 export function ViewAccount({ id, balance, owner }: ViewAccountProps) {
+  const [toggleHideBalance, setToggleHideBalance] = useState(true)
+
   return (
     <div className="flex h-screen flex-col overflow-scroll bg-[linear-gradient(215deg,_#171d26_15%,_#000_85%)]">
       <div className="mx-12">
         <header className="mt-10 flex items-center justify-between">
-          <Eye size={30} color="white" />
+          {toggleHideBalance ? (
+            <Eye
+              size={32}
+              className="cursor-pointer"
+              color="white"
+              onClick={() => setToggleHideBalance(false)}
+            />
+          ) : (
+            <EyeClosed
+              size={32}
+              className="cursor-pointer"
+              color="white"
+              onClick={() => setToggleHideBalance(true)}
+            />
+          )}
           <h1 className="text-4xl font-semibold text-white">UAB</h1>
-          <User2 size={30} color="white" />
+          <User size={32} color="white" />
         </header>
 
         <div className="mb-8 mt-16 rounded-3xl bg-white p-5">
           <div className="mt-2 flex items-center">
-            <Dot size={40} className="mt-1" color="black" />
+            <DotOutline size={38} weight="fill" className="mt-1" />
             <strong className="text-xl text-black">{id}</strong>
           </div>
 
           <div className="ml-4 mt-20">
             <h2 className="text-xl font-bold text-gray-950">Balance</h2>
-            <strong className="mt-3 block text-3xl text-black">
-              $ {balance},00
-            </strong>
+            {toggleHideBalance ? (
+              <strong className="mt-3 block text-3xl text-black">
+                $ {balance},00
+              </strong>
+            ) : (
+              <DotsThreeOutline size={48} weight="fill" />
+            )}
+
             <strong className="mt-7 block font-extrabold text-gray-800">
               {owner}
             </strong>
