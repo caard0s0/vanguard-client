d40317ff0d48773c10efbb177232da51bdd7c0c0
caard0s0 <vinicardoso216@gmail.com>
2023-10-31 10:16:41 -0300

    feat: adding more features on view account page

--- src/app/[locale]/view_account/ViewAccount.tsx
+++ src/app/[locale]/view_account/ViewAccount.tsx
@@ -2,12 +2,7 @@
 
 import { TransferHistory } from '@/components/ViewAccount/TransferHistory'
 import { deleteUserCookie } from '@/utils/userCookie'
-import {
-  Eye,
-  EyeClosed,
-  DotOutline,
-  DotsThreeOutline,
-} from '@phosphor-icons/react'
+import { Eye, EyeClosed, DotsThreeOutline } from '@phosphor-icons/react'
 import { LogOut, ArrowLeftRight } from 'lucide-react'
 import { useFormatter, useTranslations } from 'next-intl'
 import { useState } from 'react'
@@ -32,8 +27,8 @@ export function ViewAccount({
 
   return (
     <div className="flex h-screen max-w-md flex-col overflow-scroll bg-dark-blue">
-      <div>
-        <header className="mx-10 mt-10 flex items-center justify-between">
+      <div className="mx-10 mt-10">
+        <header className="flex items-center justify-between">
           {toggleHideBalance ? (
             <Eye
               size={32}
@@ -54,42 +49,40 @@ export function ViewAccount({
             <LogOut className="cursor-pointer" size={32} color="white" />
           </a>
         </header>
-
-        <div className="mt-16 bg-white p-10">
-          <div className="flex items-center">
-            <DotOutline size={38} weight="fill" />
-            <strong className="text-xl text-black">{id}</strong>
-          </div>
-
-          <div className="mt-20">
-            <h2 className="text-xl font-bold text-gray-950">
-              {viewAccountContent('balance')}
-            </h2>
-            {toggleHideBalance ? (
-              <strong className="mt-3 block text-3xl text-black">
-                {format.number(balance, {
-                  style: 'currency',
-                  currency,
-                })}
-              </strong>
-            ) : (
-              <DotsThreeOutline size={48} weight="fill" />
-            )}
-
-            <strong className="mt-7 block font-extrabold text-gray-800">
-              {owner}
-            </strong>
-          </div>
-          <a
-            href="/send_transfer"
-            className="mt-10 flex justify-center gap-2 rounded-2xl bg-black p-6 text-white hover:bg-gray-950"
-          >
-            <ArrowLeftRight color="green" className="mt-[2px]" />
-            {viewAccountContent('send_money')}
-          </a>
-          <TransferHistory owner={owner} currency={currency} />
-          <div className="mt-14"></div>
+        <div className="mt-16">
+          <span className="text-xl font-semibold text-white">
+            {viewAccountContent('welcome')} {owner}
+          </span>
         </div>
+      </div>
+
+      <div className="bg-white p-10">
+        <strong className="text-xl text-black">ID: {id}</strong>
+
+        <div className="mt-20">
+          <span className="text-xl font-bold text-gray-950">
+            {viewAccountContent('balance')}
+          </span>
+          {toggleHideBalance ? (
+            <strong className="mt-3 block text-3xl text-black">
+              {format.number(balance, {
+                style: 'currency',
+                currency,
+              })}
+            </strong>
+          ) : (
+            <DotsThreeOutline size={48} weight="fill" />
+          )}
+        </div>
+        <a
+          href="/send_transfer"
+          className="mt-7 flex justify-center gap-2 rounded-2xl bg-black p-6 text-white hover:bg-gray-950"
+        >
+          <ArrowLeftRight color="green" className="mt-[2px]" />
+          {viewAccountContent('send_money')}
+        </a>
+        <TransferHistory owner={owner} currency={currency} />
+        <div className="mt-14"></div>
       </div>
     </div>
   )
